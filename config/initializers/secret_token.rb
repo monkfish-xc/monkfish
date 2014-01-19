54c61c92f089215926e4248b42a1b2ced0b98394
atom-morgan <atom.morgan@gmail.com>
2014-01-19 18:08:24 -0500

    Initial commit with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monkfish::Application.config.secret_key_base = 'e27a67e8c35d745ffcf010f4004cbd20f9de57a63b399bf193a7667b66ef83224bf59a0dafea1233a1b5d5f1edca1c16c3328fe5cb0118ca977872c7bd7a8d4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Monkfish::Application.config.session_store :cookie_store, key: '_monkfish_session'
