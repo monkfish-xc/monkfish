80bb827a807bc48d617e3d40da784fff843e9039
atom-morgan <atom.morgan@gmail.com>
2014-01-22 15:45:54 -0500

    Add posts to user show page and create partial for post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # @posts = @user.posts.paginate(page: params[:page])
+    @posts = @user.posts
   end
 
   def new
