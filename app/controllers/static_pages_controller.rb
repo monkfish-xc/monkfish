70348de63da493887f95d0b130876cd93f0904ed
atom-morgan <atom.morgan@gmail.com>
2014-01-23 18:03:41 -0500

    Fix sign in block in StaticPages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-      @post = current_user.posts.build if signed_in?
+      @post = current_user.posts.build
       @feed_items = current_user.feed
     end
   end
