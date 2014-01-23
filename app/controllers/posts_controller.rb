1cd31aafe9db00d298f09d169bfb21d93754a68f
atom-morgan <atom.morgan@gmail.com>
2014-01-23 17:35:05 -0500

    Allow users to delete their own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :signed_in_user # app/helpers/sessions_helper.rb
+  before_action :signed_in_user, only: [:create, :destroy] # app/helpers/sessions_helper.rb
+  before_action :correct_user, only: :destroy
 
   def create
     @post = current_user.posts.build(post_params)
@@ -15,11 +16,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_url
   end
 
   private
 
     def post_params
       params.require(:post).permit(:content, :title, :post_text)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
