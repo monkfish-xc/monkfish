aaf9821e897c7dae7f4d91de60d065241b330a04
Kyle Cline <kyledcline@gmail.com>
2014-02-09 02:34:37 -0600

    Add reference to posts (children) onto user (parent)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140209080133) do
+ActiveRecord::Schema.define(version: 20140209082343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,10 @@ ActiveRecord::Schema.define(version: 20140209080133) do
     t.string   "content"
     t.string   "title"
     t.string   "leading_comment"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
-
-  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
