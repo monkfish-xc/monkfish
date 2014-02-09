8b5b8399060dfe5babf55f6f6dc3e9adcdf5eda9
Kyle Cline <kyledcline@gmail.com>
2014-02-09 01:45:08 -0600

    Add gems annotate and better_errors to group dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
+# == Route Map (Updated 2014-02-09 01:44)
+#
+#      Prefix Verb   URI Pattern               Controller#Action
+#        root GET    /                         static_pages#home
+#        help GET    /help(.:format)           static_pages#help
+#       about GET    /about(.:format)          static_pages#about
+#       users GET    /users(.:format)          users#index
+#             POST   /users(.:format)          users#create
+#    new_user GET    /users/new(.:format)      users#new
+#   edit_user GET    /users/:id/edit(.:format) users#edit
+#        user GET    /users/:id(.:format)      users#show
+#             PATCH  /users/:id(.:format)      users#update
+#             PUT    /users/:id(.:format)      users#update
+#             DELETE /users/:id(.:format)      users#destroy
+#      signup GET    /signup(.:format)         users#new
+#    sessions POST   /sessions(.:format)       sessions#create
+# new_session GET    /sessions/new(.:format)   sessions#new
+#     session DELETE /sessions/:id(.:format)   sessions#destroy
+#      signin GET    /signin(.:format)         sessions#new
+#     signout DELETE /signout(.:format)        sessions#destroy
+#       posts POST   /posts(.:format)          posts#create
+#        post DELETE /posts/:id(.:format)      posts#destroy
+#
+
 Monkfish::Application.routes.draw do
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
